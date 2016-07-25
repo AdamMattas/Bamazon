b888492e33d1a58433dc5836288bfc2a4957e2dc
adam fader <adamfader@gmail.com>
2016-07-24 22:00:44 -0400

    added more comments to files

--- BamazonExecutive.js
+++ BamazonExecutive.js
@@ -1,7 +1,7 @@
-var Table = require('cli-table');
-var inquirer = require('inquirer');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+var Table = require('cli-table'); //Require npm package
+var inquirer = require('inquirer'); //Require npm package
+var mysql = require('mysql'); //Require npm package
+var connection = mysql.createConnection({ //Create connection to local DB
   host: "localhost",
   port: 3306,
   user: "root",
@@ -9,11 +9,12 @@ var connection = mysql.createConnection({
   database: "Bamazon"
 })
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runExecutive();
+connection.connect(function(err) { //Open local DB connection
+  if (err) throw err; //Throw error if encountered
+  runExecutive(); //Run initial function
 })
 
+//Give user 2 choices for the program
 var runExecutive = function() {
   inquirer.prompt({
     name: "action",
@@ -24,6 +25,7 @@ var runExecutive = function() {
       "Create New Department"
     ]
   }).then(function(answer) {
+    //Check the response and execute the appropriate function
     switch(answer.action) {
       case 'View Product Sales by Department':
           viewProducts();
@@ -36,32 +38,43 @@ var runExecutive = function() {
   });
 }
 
+//Retrieves all entries in the Departments table
 var viewProducts = function() {
-
+  //Initialize and set query parameters
   var queryAll = 'SELECT * FROM Departments';
- 
+  //Execute the query
   connection.query(queryAll, function(err, rows, fields) {
-    if (err) throw err;
-
+    if (err) throw err; //Throw error if encountered
+    //Creates a new table from the cli-table npm package
     var table = new Table({
+        //Give labels to the head of each column
         head: ['DepartmentID', 'DepartmentName', 'OverHeadCosts', 'ProductSales', 'TotalProfit']
+        //Specifies the width of each column in table
       , colWidths: [15, 17, 15, 15, 15]
     });
-
+    //Loop through the response
     for (var i in rows) {
+      //Calculate dept profit by subtracting sales from overhead costs
       var profit = parseFloat(rows[i].TotalSales) - parseFloat(rows[i].OverHeadCosts);
+      //Push each row from DB table to display table just created
       table.push(
           [rows[i].DepartmentID, rows[i].DepartmentName, rows[i].OverHeadCosts, rows[i].TotalSales, profit]
       );
     }
 
+    //Display the table in the console
     console.log(table.toString());
+    console.log("");
+    //Call askUser function
+    askUser();
 
   });
 
 }
 
+//Allows the user to create a new department in the Departments table
 var createDept = function() {
+  //Get dept name and overhead cost for dept
   inquirer.prompt([
     {
       type: "input",
@@ -74,15 +87,52 @@ var createDept = function() {
       message: "Enter the overhead cost for this department."
     }
   ]).then(function(answer) {
+      //Execute query with parameters from user input
       connection.query("INSERT INTO Departments SET ?", {
         DepartmentName: answer.name,
         OverHeadCosts: answer.overhead,
-        TotalSales: 0.00
+        TotalSales: 0.00 //Starts dept at 0 sales
       }, function(err, res) {
-        if (err) throw err;
+        if (err) throw err; //Throw error if encountered
+        //Tell the user addidng dept was successful
         console.log("Your new department was added successfully!");
+        console.log("");
+        //Call askUser function
+        askUser();
       });
 
   })
+
+}
+
+//Asks the user to continue or exit program
+var askUser = function() {
+  //Ask user if they wish to continue
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "Would you like to run this program again?",
+    choices: [
+      'Yes', 
+      "No"
+    ]
+  }).then(function(answer) {
+    //Check the response and execute the appropriate function
+    switch(answer.action) {
+      case 'Yes':
+          runExecutive();
+      break;
+
+      case 'No':
+          endProgram();
+      break;
+    }
+  });
+}
+
+//Close the DB connection
+var endProgram = function() {
+
+  connection.end();
 
 }
--- BamazonManager.js
+++ BamazonManager.js
@@ -1,6 +1,6 @@
-var inquirer = require('inquirer');
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+var inquirer = require('inquirer'); //Require npm package
+var mysql = require('mysql'); //Require npm package
+var connection = mysql.createConnection({ //Create connection to local DB
   host: "localhost",
   port: 3306,
   user: "root",
@@ -8,11 +8,12 @@ var connection = mysql.createConnection({
   database: "Bamazon"
 })
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runManager();
+connection.connect(function(err) { //Open local DB connection
+  if (err) throw err; //Throw error if encountered
+  runManager(); //Run initial function
 })
 
+//Give user 4 choices for the program
 var runManager = function() {
   inquirer.prompt({
     name: "action",
@@ -25,6 +26,7 @@ var runManager = function() {
       "Add New Product"
     ]
   }).then(function(answer) {
+    //Check the response and execute the appropriate function
     switch(answer.action) {
       case 'View Products for Sale':
           viewProducts();
@@ -45,66 +47,85 @@ var runManager = function() {
   });
 }
 
+//Shows user all products for sale
 var viewProducts = function() {
+  //Initialize and set query parameters
   var query = 'SELECT * FROM Products';
- 
+  //Execute the query
   connection.query(query, function(err, rows, fields) {
-    if (err) throw err;
-
+    if (err) throw err; //Throw error if encountered
+    //Loop through the rows returned and log them to the console
     for (var i in rows) {
         console.log('Product ID: ', rows[i].ItemID, ' Product Name: ', rows[i].ProductName, ' Price: ', rows[i].Price, ' Available: ', rows[i].StockQuantity);
     }
+    //Call askUser function
+    askUser();
   });
 
 }
 
-// If a manager selects option 2 it should list all items for which the quantity available in stores is less than 5.
+//Shows user products with 5 or less left in stock
 var viewLow = function() {
+  //Initialize and set query parameters
   var query = 'SELECT * FROM Products WHERE StockQuantity <= 5';
- 
+  //Execute the query
   connection.query(query, function(err, rows, fields) {
-    if (err) throw err;
-
+    if (err) throw err; //Throw error if encountered
+    //Loop through the rows returned and log them to the console
     for (var i in rows) {
         console.log('Product ID: ', rows[i].ItemID, ' Product Name: ', rows[i].ProductName, ' Price: ', rows[i].Price, ' Available: ', rows[i].StockQuantity);
     }
+    //Call askUser function
+    askUser();
   });
 
 }
 
+//Allow the user to add inventory for products
 var addInventory = function() {
+  //Execute DB query
   connection.query('SELECT * FROM Products', function(err, res) {
-    console.log(res);
+    //List items are returned from DB query
     inquirer.prompt({
       name: "choice",
       type: "list",
       choices: function(value) {
+        //Create empty array to hold list items
         var choiceArray = [];
+        //Loop through response and push all product names to the choiceArray
         for (var i = 0; i < res.length; i++) {
             choiceArray.push(res[i].ProductName);
         }
+        //Sets choiceArray as list items
         return choiceArray;
       },
       message: "Which item would you like to increase the quantity of?"
     }).then(function(answer) {
+        //Loop through response for a match to user input
         for(var i = 0; i < res.length; i++) {
           if(res[i].ProductName == answer.choice) {
+            //Set entire product row as var for UPDATE reference
             var chosenItem = res[i];
+            //Set current specific product inventory amount as var to be added to the new quantity later
             var addQuantity = res[i].StockQuantity;
-            //console.log(addQuantity);
+            //Ask user how much inventory to add
             inquirer.prompt({
                 name: "add",
                 type: "input",
                 message: "How many would you like to add?"
             }).then(function(answer) {
+                //Add the new inventory with the current inventory and set the sum as a var
                 var addedQuant = parseInt(addQuantity) + parseInt(answer.add);
+                //Execute query with the inventory amount and itemId as parameters
                 connection.query("UPDATE Products SET ? WHERE ?", [{
                   StockQuantity: addedQuant
                 }, {
                   ItemID: chosenItem.ItemID
                 }], function(err, res) {
+                  //Tell the user quantity update was successful
                   console.log("Quantity updated successfully!");
-                  //start();
+                  //Call askUser function
+                  askUser();
                 });
             })
           }
@@ -114,7 +135,9 @@ var addInventory = function() {
 
 }
 
+//Allows user to add a new product to DB
 var addNewProduct = function() {
+  //Get product name, dept, price and quantity
   inquirer.prompt([
     {
       type: "input",
@@ -137,16 +160,51 @@ var addNewProduct = function() {
       message: "Enter the quantity available for this product."
     }
   ]).then(function(answer) {
+      //Execute query with parameters from user input
       connection.query("INSERT INTO Products SET ?", {
             ProductName: answer.name,
             DepartmentName: answer.department,
             Price: answer.price,
             StockQuantity: answer.quantity
         }, function(err, res) {
+          //Tell the user addidng product was successful
             console.log("Your new product was added successfully!");
-            runManager();
+            //Call askUser function
+            askUser();
         });
 
   })
+
+}
+
+//Asks the user to continue or exit program
+var askUser = function() {
+  //Ask user if they wish to continue
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "Would you like to run this program again?",
+    choices: [
+      'Yes', 
+      "No"
+    ]
+  }).then(function(answer) {
+    //Check the response and execute the appropriate function
+    switch(answer.action) {
+      case 'Yes':
+          runManager();
+      break;
+
+      case 'No':
+          endProgram();
+      break;
+    }
+  });
+}
+
+//Close the DB connection
+var endProgram = function() {
+
+  connection.end();
 
 }
